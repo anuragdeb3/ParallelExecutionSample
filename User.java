8d43fbebbc071928b6f3fd617ff5cad6269a9ae3
Anurag Deb Roy <anuragdeb8@gmail.com>
2025-06-13 19:04:33 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,9 @@
+public class User {
+    public String userId;
+    public String password;
+
+    public User(String userId, String password) {
+        this.userId = userId;
+        this.password = password;
+    }
+}
