2dd5f93d74359f500cf6ec3dd4e1ce04718123ed
Anurag Deb Roy <anuragdeb8@gmail.com>
2025-06-13 18:49:18 +0530

    Update BaseFactory.java

--- BaseFactory.java
+++ BaseFactory.java
@@ -3,7 +3,7 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class BaseTest {
+public class BaseFactory {
     protected WebDriver driver;
 
     public void setUp() {
